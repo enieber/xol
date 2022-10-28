57609f64f3a6fc9d295d6b82efd8cb7233f3bb7c
enieber cunha <eniebercunha@gmail.com>
2022-10-28 17:22:25 -0300

    feat: remove dead code

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -3,8 +3,7 @@ import { Contract } from "@ethersproject/contracts";
 import { shortenAddress, useCall, useEthers, useLookupAddress } from "@usedapp/core";
 import React, { useEffect, useState } from "react";
 
-import { Body, Button, Container, Header, Image, Link } from "./components";
-import logo from "./ethereumLogo.png";
+import { Button } from "./components";
 
 import { addresses, abis } from "@my-app/contracts";
 import GET_TRANSFERS from "./graphql/subgraph";
@@ -69,22 +68,10 @@ function App() {
   }, [loading, subgraphQueryError, data]);
 
   return (
-    <Container>
-      <Header>
-        <WalletButton />
-      </Header>
-      <Body>
-        <Image src={logo} alt="ethereum-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        <Link href="https://reactjs.org">
-          Learn React
-        </Link>
-        <Link href="https://usedapp.io/">Learn useDapp</Link>
-        <Link href="https://thegraph.com/docs/quick-start">Learn The Graph</Link>
-      </Body>
-    </Container>
+    <div>
+      <h2>Bom dia</h2>
+      <WalletButton />
+    </div>
   );
 }
 
